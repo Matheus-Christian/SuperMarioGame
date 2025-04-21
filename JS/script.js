763fc951c1c410882594db7ffee648387f0cdc58
Matheus <matheuschristian.com@gmail.com>
2025-04-21 12:58:30 -0300

    first part: completed

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe')
 
 const jump = () => { /*2 - dispara a função jump*/
     mario.classList.add('jump'); /*3 -  add class jump na class mario */
@@ -9,3 +10,23 @@ const jump = () => { /*2 - dispara a função jump*/
 }
 
 document.addEventListener("keydown", jump); /*1 - ao pressionar qualquer tecla */
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation ='nome';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './IMG/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop)
+    }
+}, 10);
